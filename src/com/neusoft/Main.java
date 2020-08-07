2e9844809ab927c7e7601d679a7b74020160fc52
liying588 <68541214+liying588@users.noreply.github.com>
2020-08-07 14:07:50 +0800

    111

---
+++ src/com/neusoft/Main.java
@@ -0,0 +1,7 @@
+package com.neusoft;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
---
+++ src/com/neusoft/dao/Impl/AdminDAOImpl.java
@@ -0,0 +1,14 @@
+package com.neusoft.dao.Impl;
+
+import com.neusoft.dao.AdminDao;
+import com.neusoft.domain.Admin;
+
+public class AdminDAOImpl implements AdminDao {
+    @Override
+    public Admin getAdminByNameByPass(String adminName, String password) {
+
+
+
+        return null;
+    }
+}
